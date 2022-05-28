ae44a3dc2c2d6490cdd5ad0fd7e732e03aad5a18
João Paulo <joaopdejesus79@gmail.com>
2022-05-28 09:30:57 -0300

    Tarefa NomeCompletoJS Finalizada

---
+++ NomeCompleto.js
@@ -0,0 +1,5 @@
+const nome = prompt ("Insira o seu primeiro nome:");
+const sobrenome = prompt ("Insira o seu sobrenome:");
+
+alert(`Nome Completo: ${nome} ${sobrenome}`);
+alert(`Nome Catálogo: ${sobrenome.toUpperCase()}, ${nome}`);
